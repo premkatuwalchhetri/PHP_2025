b2939e5555f1d080dc6e167e59f3763b7d4cbbe1
PREM BAHADUR K.C. <bahadurkcprem1@gmail.com>
2025-06-13 04:04:30 -0400

    Delete Assignment_one directory

--- Assignment_one/sql/sql.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE students (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(200) NOT NULL,
-    email VARCHAR(35) NOT NULL,
-    student_id VARCHAR(30) NOT NULL,
-    course VARCHAR(50) NOT NULL,
-    grade VARCHAR(20) NOT NULL
-);
